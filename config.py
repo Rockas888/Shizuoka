c25868e158996f5891eb7b4e777c10f0d1d95e94
Rockas888 <105117889+Rockas888@users.noreply.github.com>
2024-02-19 16:21:11 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# (c) ACE 
+
+import os
+
+class Config(object):
+    # get a token from @BotFather
+    BOT_TOKEN = "6941983097:AAGciD0zqHUa9OBY5xLbImM7AKLE_dDlDVk"
+    API_ID = "23282918"
+    API_HASH = "ccf2a6664d1ae478b8c556000f616120"
+    AUTH_USERS = '5911618569'
+    
